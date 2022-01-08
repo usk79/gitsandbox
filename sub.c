2064837e0c98ceddb49196e69ea4cc48ee03cb2e
Yusuke <yusuke.m79@gmail.com>
2022-01-08 23:58:04 +0900

    merge

--- sub.c
+++ sub.c
@@ -6,13 +6,6 @@ int sub_func()
     printf("this is sub_func()");
     printf("abcdefg\n")
 }
-<<<<<<< HEAD
-/*
-int main()
-{
-    sub_func();
-}*/
-=======
 
 #ifdef TEST
 int main()
@@ -20,4 +13,3 @@ int main()
     sub_func();
 }
 #endif
->>>>>>> 44bece1f1db085ced274af3bdfebaa782f8cee47
